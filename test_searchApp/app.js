6f4885325983eb4158055e0c9505b2d8e3bb1936
Eduard Golovachuk <eduard.golovachuk@gmail.com>
2020-03-26 15:35:48 +0000

    /search endpoint added

--- test_searchApp/app.js
+++ test_searchApp/app.js
@@ -4,10 +4,17 @@ var request = require ("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+	res.render("search");
+});
+
 //
 app.get("/results",(req, res) => {
-	request("http://www.omdbapi.com/?s=california&apikey=thewdb", (error, response, body) => {
+	var s = req.query.search; //"search" comes from search.ejs name="search"
+	var url = "http://www.omdbapi.com/?s="+s+"&apikey=thewdb";
+	request(url, (error, response, body) => {
 		if (!error && response.statusCode == 200) {
+			console.log(body);
 			var data = JSON.parse(body);
 			res.render("results", {data: data});
 		}
