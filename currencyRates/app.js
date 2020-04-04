0be4570a62cd43563535d48f8d65f4e91525ade8
Eduard Golovachuk <eduard.golovachuk@gmail.com>
2020-04-04 23:20:05 +0000

    http.get added

--- currencyRates/app.js
+++ currencyRates/app.js
@@ -1,20 +1,33 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    http = require("http");
 
 app.set("view engine","ejs");
+app.use(express.static("public"));//to use separate folder for css and other scripts files
+app.use(bodyParser.urlencoded({extended: true})) //telling express to use body-parser
 
 //chart route
-app.get("/", (req,res) => {
-	//code here
-	var url = "http://www.omdbapi.com/?s=california&apikey=thewdb";
-	request(url, (error, response, body)=>{
-		if(!error && response.statusCode == 200){
-			var data = JSON.parse(body);
-			console.log(data);
-			res.render("home", {data: data});
-		}
-	})
+app.get("/", (req, res) => {
+	
+	var url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=12/03/2020&date_req2=16/03/2020&VAL_NM_RQ=R01235";
+	
+	http.get(url, res => {
+		
+		res.setEncoding("utf8");
+			let body = "";
+		
+		res.on("data", data => {
+			body += data;
+			console.log(`BODY START ${body} BODY END \n`)
+			});
+		
+		// res.on("end", () => {
+		// body = JSON.parse(body);
+		// 	});
+	});
+	
+	res.render("home");
 });
 
 app.listen(3000, () => {
--- currencyRates/public/chart.js
+++ currencyRates/public/chart.js
@@ -1,16 +1,16 @@
 //Chart script provided by Apexchart (https://apexcharts.com/docs/update-charts-from-json-api-ajax/#)
 
-window.onload = function() {
-	//getting data from the JSON logic
-	var url = 'rates.json';
+window.onload = () => {
+//getting data from the JSON logic
+	var url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=12/03/2020&date_req2=16/03/2020&VAL_NM_RQ=R01235";
 	var dataPoints = [];
- 
-	$.getJSON(url, function(response) {
-		// console.log(response.Record);
-		response.Record.forEach(function(pair){
-			var x = pair._Date;
-			var y = pair.Value;
-			
+
+	$.get(url, (data) => {
+		$(data).find("Record").each(()=> {
+			var $dataPoint = $(this);
+			var x = $dataPoint.attr("Date");
+			var y = $dataPoint.find("Value").text();
+
 			// Splicing "y" into two parts, removing the textual comma from "y" 
 			// and replacing it with numeric comma
 			var comma = y.search(",");
@@ -19,18 +19,50 @@ window.onload = function() {
 			var ySecond = y.slice(comma+1,-1);
 			// Inserting the numeric comma
 			var yBoth = yFirst+"."+ySecond;
+	
+			//Just debugging
+			//console.log("Type of xBoth: "+typeof(yBoth)+" "+yBoth);
+			
+			//Pushing into the array
+			dataPoints.push({x: parseFloat(x), y: parseFloat(yBoth)});
+	
+	
+	// var url = 'rates.json';
+	// var dataPoints = [];
+ 
+	// $.getJSON(url, function(response) {
+	// 	// console.log(response.Record);
+	// 	response.Record.forEach(function(pair){
+	// 		var x = pair._Date;
+	// 		var y = pair.Value;
+			
+	// 		// Splicing "y" into two parts, removing the textual comma from "y" 
+	// 		// and replacing it with numeric comma
+	// 		var comma = y.search(",");
+	// 		// Getting the textual comma position and  "y" partitioning
+	// 		const yFirst = y.slice(0,comma); 
+	// 		var ySecond = y.slice(comma+1,-1);
+	// 		// Inserting the numeric comma
+	// 		var yBoth = yFirst+"."+ySecond;
 		
-			//Pushing an object to the dataPoints array parsing the floating value
-			dataPoints.push({x: x, y: parseFloat(yBoth)});
-			});
-		
+	// 		//Pushing an object to the dataPoints array parsing the floating value
+	// 		dataPoints.push({x: x, y: parseFloat(yBoth)});
+	// 		});
+			}
+		)
+	});
 			//Updating the 'series' array of var options below with given dataPoints
-			chart.updateSeries([{
-				name: 'Rates',
-				data: dataPoints
-			}]);
+			chart.updateSeries(
+		  		[
+		  			{
+						name: 'Rates',
+						data: dataPoints
+					}
+		  		]
+		 );
+	console.log(dataPoints);
 
-		});
+		
 	
 
 	var options = {
@@ -56,7 +88,7 @@ window.onload = function() {
 	);
 	
 	chart.render();
-  
+}
 	
 	// var url = 'rates.json';
 
@@ -151,7 +183,7 @@ window.onload = function() {
 // 			var ySecond = y.slice(comma+1,-1);
 // 			// Inserting the numeric comma
 // 			var yBoth = yFirst+"."+ySecond;
-// 			//Just debigging
+// 			//Just debugging
 // 			console.log("Type of xBoth: "+typeof(yBoth)+" "+yBoth);
 // 			dataPoints.push({x: parseFloat(x), y: parseFloat(yBoth)});
 
@@ -215,4 +247,3 @@ window.onload = function() {
 		
 // 		chart.render();
 // 	});
-} 
