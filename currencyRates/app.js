96dc3b9e18e3723bdac131bd3adcc89272d3354d
Eduard Golovachuk <eduard.golovachuk@gmail.com>
2020-04-05 17:50:49 +0000

    xml2json parser + parsing logic added. json object is returned to the server console

--- currencyRates/app.js
+++ currencyRates/app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    http = require("http");
+    http = require("http"),
+	xmlParser = require("xml2json"); //parses XML string to a JSON object
 
 app.set("view engine","ejs");
 app.use(express.static("public"));//to use separate folder for css and other scripts files
@@ -11,23 +12,28 @@ app.use(bodyParser.urlencoded({extended: true})) //telling express to use body-p
 app.get("/", (req, res) => {
 	
 	var url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=12/03/2020&date_req2=16/03/2020&VAL_NM_RQ=R01235";
+	var body = "";
+	var jsonBody;
 	
 	http.get(url, res => {
 		
 		res.setEncoding("utf8");
-			let body = "";
 		
 		res.on("data", data => {
 			body += data;
-			console.log(`BODY START ${body} BODY END \n`)
+			//console.log(`XML `, typeof(body), `: ${body}`)
 			});
-		
-		// res.on("end", () => {
-		// body = JSON.parse(body);
-		// 	});
+		 
+		res.on("end", () => {
+			
+			jsonBody = xmlParser.toJson(body);
+			jsonBody = JSON.parse(jsonBody);
+			console.log(jsonBody);
+			}
+		)
 	});
 	
-	res.render("home");
+	res.render("home", {"jsonBody": jsonBody});
 });
 
 app.listen(3000, () => {
